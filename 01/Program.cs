89b2fc53abce6995c34bf183ff033c687506d496
Антон <dgamik_140787@mail.ru>
2022-08-06 19:13:12 +0300

    01

---
+++ 01/Program.cs
@@ -0,0 +1,14 @@
+﻿// int fioat bool double
+//int a =0;
+//float b = 5.2f;
+//string c = "hello";
+//bool d = false; // true
+//double e = 10000000000.2f;
+
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(number1+number2);
+
+
